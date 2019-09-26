7a8ebf6d570f899fc8989042a5c69a10635219ed
Alex <thealex11011999@gmail.com>
2019-09-26 14:26:58 +0200

    File reader

--- src/Ejercicio10.kt
+++ src/Ejercicio10.kt
@@ -1,2 +1,3 @@
 //Crea un programa que pide al usuario una lista de 10 números enteros.
 // Posteriormenteordena la lista creando tu propio algoritmo de ordenación e imprime por pantalla la lista demenor a mayor.
+
--- src/Ejercicio11.kt
+++ src/Ejercicio11.kt
@@ -1,3 +1,10 @@
+import java.io.File
+
 //Mejora el programa anterior de forma que la lista de números está almacenada en un fichero.
 // El programa acepta como primer argumento la ruta de dicho fichero.
-// Controla los posibleserrores.
+// Controla los posibles errores.
+
+fun main() {
+    var lineas: List<String> = (File("/rutaAbsoluta").readLines())
+
+}
